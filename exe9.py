8ccaaa6ad8e1c97a9ee8a9a3347274333cba2ae5
Fernando Ferreira <119910757+NaNd051@users.noreply.github.com>
2023-08-06 20:25:30 -0300

    tabuada
    exe009 tabuada

---
+++ exe9.py
@@ -0,0 +1,4 @@
+num = int(input('digite o numero da sua tabuada:'))
+print('{} x {} = {}'.format(num, 1, num*1))
+print('{} x {} = {}'.format(num, 2, num*2))
+print('{} x {} = {}'.format(num, 3, num*3))
